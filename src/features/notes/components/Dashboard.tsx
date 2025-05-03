f7fd6b8b2022b6d9fa61db5086de775e2b6bff17
Jummate <yakubjumat@gmail.com>
2025-05-03 18:25:58 +0100

    feat: complete empty part of All Notes

--- src/features/notes/components/Dashboard.tsx
+++ src/features/notes/components/Dashboard.tsx
@@ -11,11 +11,11 @@ function Dashboard() {
   // const navigate = useNavigate();
   return (
     <div className="min-h-screen cont flex flex-col">
-      <div className="lg:hidden relative flex-1">
-        This is mobile
+      <div className="lg:hidden relative flex flex-col flex-1">
+        <Outlet />
         <Footer />
       </div>
-      <div className="hidden lg:grid grid-cols-[300px_1fr] flex-1">
+      <div className="hidden lg:grid grid-cols-[250px_1fr] flex-1">
         <Sidebar />
         <main className="grid grid-rows-[100px_1fr]">
           <Header />
--- src/features/notes/pages/ArchivedNotes.tsx
+++ src/features/notes/pages/ArchivedNotes.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function ArchivedNotes() {
   return (
-    <div>
-      <div className="grid grid-cols-[1fr_2fr_1fr] h-[90%]">
+    <div className="flex-1 flex flex-col">
+      <div className="grid grid-cols-[1fr_2fr_1fr]">
         <div className="p-10">Archived Notes</div>
         <div className="">2</div>
         <div className="">3</div>
