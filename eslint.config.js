f9aa1e3b4734c32a3b9263471f550880587fe995
Jummate <yakubjumat@gmail.com>
2025-06-01 09:11:24 +0100

    chore(pre-commit): move pre-commit commands from package.json into a config file

--- eslint.config.js
+++ eslint.config.js
@@ -8,6 +8,9 @@ import prettierConfig from 'eslint-config-prettier';
 // Your config will be an array of objects
 export default [
   {
+    ignores: ['**/node_modules/**', '**/dist/**', '**/build/**', '.github/'],
+  },
+  {
     // Specify language options for TypeScript and JSX files
     files: ['**/*.ts', '**/*.tsx'],
     languageOptions: {
