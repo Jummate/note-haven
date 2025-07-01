c1b149bafd25b37c6581420805f0e34cd01be56e
Jummate <yakubjumat@gmail.com>
2025-07-01 21:16:29 +0100

    feat(editor):add auto-resize textarea to editor

--- src/features/notes/components/NoteEditor.tsx
+++ src/features/notes/components/NoteEditor.tsx
@@ -1,25 +1,25 @@
+import { useState } from 'react';
 import { Input } from '../../../shared/components';
 import { AppIcons } from '../../../shared/icons/Icons';
 import TagSelect from '../../tags/components/TagSelect';
+import AutoResizeTextarea from '../../../shared/components/AutoResizeTextArea';
 
 function NoteEditor() {
   const TagIcon = AppIcons['tags'];
   const ClockIcon = AppIcons['clock'];
+  const [content, setContent] = useState<string>('');
   return (
     <section className="w-full">
       <Input
-        styles="text-3xl placeholder:text-black placeholder:font-bold font-inter border-none text-black font-bold font-inter"
+        styles="text-3xl placeholder:text-black placeholder:font-bold font-inter border-none text-black font-bold font-inter mb-3"
         placeholder="Enter a title..."
       />
-      <section className="flex flex-col gap-4 text-secondary-700">
-        <div className="grid grid-cols-[120px_1fr] items-start gap-2">
+      <section className="flex flex-col gap-6 text-secondary-700">
+        <div className="grid grid-cols-[120px_1fr] items-center">
           <span>
             <TagIcon className="inline text-secondary-500" /> Tags
           </span>
-          {/* <span>{note.tags.map((item) => item.name).toString()}</span> */}
-          <span className="flex flex-wrap gap-2">
-            <TagSelect />
-          </span>
+          <TagSelect />
         </div>
         <div className="grid grid-cols-[120px_1fr]">
           <span className="flex items-center gap-2">
@@ -27,7 +27,9 @@ function NoteEditor() {
           </span>
           <span className="text-secondary-500">Not yet started</span>
         </div>
-        <hr className="border-secondary-200 mb-6" />
+        <hr className="border-secondary-200" />
+        {/* <Editor value={content} onChange={setContent} /> */}
+        <AutoResizeTextarea value={content} onChange={setContent} />
       </section>
     </section>
   );
